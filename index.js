d941088f4a706674a9106d85b2e5490b6bd2e2fe
TusharDahule <tushardahule@gmail.com>
2022-06-22 20:24:19 -0700

    synchronous and asynchronous callback added

--- index.js
+++ index.js
@@ -3,4 +3,35 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Callback in JS</h1>`;
+
+/* synchronous callback example */
+function callbackFunction() {
+  console.log('callback function called !');
+}
+
+function greet(funcAsArgument) {
+  console.log('inside greet function');
+  funcAsArgument();
+}
+
+greet(callbackFunction);
+
+/* asynchronous callback example */
+
+setTimeout(callbackFunction, 3000);
+
+setTimeout(function () {
+  console.log('Hey, I am anonymous function');
+}, 6000); /* callback function as anonymous function */
+
+setTimeout(() => {
+  console.log('Hey ! I am arrow function');
+}, 9000); /* callback function as arrow function */
+
+document
+  .querySelector('#callerButton')
+  .addEventListener(
+    'click',
+    callbackFunction
+  ); /* callback used in event declarations */
